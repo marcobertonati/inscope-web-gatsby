32b962c59681fd22331e8b7c4f26707ab00bdbb5
Marco Bertonati <marko.bertonati@gmail.com>
2022-04-05 18:09:25 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  siteMetadata: {
+    title: `inscope-web`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-postcss", {
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": "G-LLJ44WTDGD"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
